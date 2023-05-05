07db8830320b1892f06bc988724cacbd6a4ca784
guribaby <122307456+guribaby@users.noreply.github.com>
2023-05-04 20:59:03 -0600

    Create Programa.java

---
+++ Programa.java
@@ -0,0 +1,9 @@
+public class Programa {
+    public static void main(String[] args) {
+
+    	lecturaEmail miLectorExamen = new lecturaEmail();
+        miLectorExamen.ejecutar(176232);
+
+
+    }
+}
